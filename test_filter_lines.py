5890ea03d9d955efc78ea98de9e5837bf3d70fc5
OlenaSkakun-IPZ-22 <olena_skakunn@knu.ua>
2025-03-26 13:05:02 +0200

    Виправлено стиль коду згідно з PEP8

--- test_filter_lines.py
+++ test_filter_lines.py
@@ -1,12 +1,14 @@
 import pytest
 from filter_lines import filter_lines
 
+
 @pytest.fixture
 def sample_file(tmp_path):
     content = "apple\nbanana\napple pie\norange\ngrape juice\napple tart\n"
     input_file = tmp_path / "test_input.txt"
     input_file.write_text(content)
     return input_file
+
 
 @pytest.mark.parametrize("keyword,expected", [
     ("apple", ["apple\n", "apple pie\n", "apple tart\n"]),
